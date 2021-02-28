13f96a84f8d25a5c6c884b4c05751373a25e0ea5
Tianchi Zhang <tianchi.zhang01@gmail.com>
2021-02-27 23:59:15 -0500

    Upload queries sql file which is one of the main homework requirements. Part of 'Data Analysis' homework requirement, including 8 SQL queries answering 8 questions...

---
+++ EmployeeSQL/analysis_queries.sql
@@ -0,0 +1,50 @@
+-- 1. List the following details of each employee: employee number, last name, first name, sex, and salary.
+SELECT e.emp_no, e.last_name, e.first_name, e.sex, s.salary
+FROM employees AS e
+JOIN salaries AS s
+ON e.emp_no = s.emp_no;
+
+-- 2. List first name, last name, and hire date for employees who were hired in 1986.
+SELECT first_name, last_name,
+EXTRACT(YEAR FROM hire_date) AS "year_hired"
+FROM employees
+WHERE EXTRACT(YEAR FROM hire_date) = 1986;
+
+-- 3. List the manager of each department with the following information: department number, department name, the manager's employee number, last name, first name.
+SELECT dm.dept_no, d.dept_name, dm.emp_no, e.last_name, e.first_name
+FROM dept_manager AS dm
+JOIN departments AS d ON dm.dept_no = d.dept_no
+JOIN employees AS e ON dm.emp_no = e.emp_no;
+
+-- 4. List the department of each employee with the following information: employee number, last name, first name, and department name.
+SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
+FROM employees AS e
+JOIN dept_emp AS dm ON e.emp_no = dm.emp_no
+JOIN departments AS d ON dm.dept_no = d.dept_no;
+
+-- 5. List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
+SELECT first_name, last_name, sex
+FROM employees
+WHERE first_name = 'Hercules'
+	AND last_name LIKE 'B%';
+
+-- 6. List all employees in the Sales department, including their employee number, last name, first name, and department name.
+SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
+FROM departments AS d
+JOIN dept_emp AS dm ON d.dept_no = dm.dept_no
+JOIN employees AS e ON dm.emp_no = e.emp_no
+WHERE d.dept_name = 'Sales';
+
+-- 7. List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
+SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
+FROM departments AS d
+JOIN dept_emp AS dm ON d.dept_no = dm.dept_no
+JOIN employees AS e ON dm.emp_no = e.emp_no
+WHERE d.dept_name = 'Sales' OR d.dept_name = 'Development';
+
+-- 8. In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
+SELECT last_name, COUNT(*)
+FROM employees
+GROUP BY last_name
+ORDER BY COUNT(*) DESC;
+
