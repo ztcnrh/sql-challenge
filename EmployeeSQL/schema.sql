00cc424a5d7235d9d29ffb99a53827f13a818ae6
Tianchi Zhang <tianchi.zhang01@gmail.com>
2021-03-03 22:33:15 -0500

    Update schema.sql... So the SQL queries for creating tables are "handwritten" with personal code style preferences instead of the previous version which was generated automatically by "QuickDBD" ERD tool.

--- EmployeeSQL/schema.sql
+++ EmployeeSQL/schema.sql
@@ -1,73 +1,49 @@
-﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
--- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
 
-CREATE TABLE "employees" (
-    "emp_no" INT   NOT NULL,
-    "emp_title_id" VARCHAR   NOT NULL,
-    "birth_date" DATE   NOT NULL,
-    "first_name" VARCHAR(50)   NOT NULL,
-    "last_name" VARCHAR(50)   NOT NULL,
-    "sex" VARCHAR(20)   NOT NULL,
-    "hire_date" DATE   NOT NULL,
-    CONSTRAINT "pk_employees" PRIMARY KEY (
-        "emp_no"
-     )
+CREATE TABLE titles
+(
+    title_id VARCHAR PRIMARY KEY,
+    title VARCHAR(50) NOT NULL
 );
 
-CREATE TABLE "departments" (
-    "dept_no" VARCHAR   NOT NULL,
-    "dept_name" VARCHAR   NOT NULL,
-    CONSTRAINT "pk_departments" PRIMARY KEY (
-        "dept_no"
-     )
+CREATE TABLE employees
+(
+    emp_no INT PRIMARY KEY,
+    emp_title_id VARCHAR NOT NULL,
+    birth_date DATE NOT NULL,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    sex VARCHAR(20) NOT NULL,
+    hire_date DATE NOT NULL,
+    FOREIGN KEY (emp_title_id) REFERENCES titles(title_id)
 );
 
-CREATE TABLE "dept_emp" (
-    "emp_no" INT   NOT NULL,
-    "dept_no" VARCHAR   NOT NULL,
-    CONSTRAINT "pk_dept_emp" PRIMARY KEY (
-        "emp_no","dept_no"
-     )
+CREATE TABLE departments
+(
+    dept_no VARCHAR PRIMARY KEY,
+    dept_name VARCHAR NOT NULL
 );
 
-CREATE TABLE "dept_manager" (
-    "dept_no" VARCHAR   NOT NULL,
-    "emp_no" INT   NOT NULL,
-    CONSTRAINT "pk_dept_manager" PRIMARY KEY (
-        "dept_no","emp_no"
-     )
+CREATE TABLE dept_emp
+(
+    emp_no INT NOT NULL,
+    dept_no VARCHAR NOT NULL,
+    PRIMARY KEY(emp_no,dept_no),
+    FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+    FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
 );
 
-CREATE TABLE "salaries" (
-    "emp_no" INT   NOT NULL,
-    "salary" INT   NOT NULL,
-    CONSTRAINT "pk_salaries" PRIMARY KEY (
-        "emp_no"
-     )
+CREATE TABLE dept_manager
+(
+    dept_no VARCHAR NOT NULL,
+    emp_no INT NOT NULL,
+    PRIMARY KEY(dept_no,emp_no),
+    FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
+    FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
 );
 
-CREATE TABLE "titles" (
-    "title_id" VARCHAR   NOT NULL,
-    "title" VARCHAR(50)   NOT NULL,
-    CONSTRAINT "pk_titles" PRIMARY KEY (
-        "title_id"
-     )
+CREATE TABLE salaries
+(
+    emp_no INT PRIMARY KEY,
+    salary INT NOT NULL,
+    FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
 );
-
-ALTER TABLE "employees" ADD CONSTRAINT "fk_employees_emp_title_id" FOREIGN KEY("emp_title_id")
-REFERENCES "titles" ("title_id");
-
-ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_emp_no" FOREIGN KEY("emp_no")
-REFERENCES "employees" ("emp_no");
-
-ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_dept_no" FOREIGN KEY("dept_no")
-REFERENCES "departments" ("dept_no");
-
-ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_dept_no" FOREIGN KEY("dept_no")
-REFERENCES "departments" ("dept_no");
-
-ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_emp_no" FOREIGN KEY("emp_no")
-REFERENCES "employees" ("emp_no");
-
-ALTER TABLE "salaries" ADD CONSTRAINT "fk_salaries_emp_no" FOREIGN KEY("emp_no")
-REFERENCES "employees" ("emp_no");
